2036434356da014f922dfef49d9e140d7044debc
Álvaro França Fernandes <alvarofrancafranca@gmail.com>
2022-06-06 15:29:24 -0300

    POO - Aula 98

--- classes.py
+++ classes.py
@@ -2,7 +2,17 @@ from pessoa import Pessoa
 
 # estamos utilizando um molde para criar uma pessoa
 p1 = Pessoa('Alvalu', 22)  # Pessoa() seria o molde # p1 = instancia
-p1.comer('maçã')
+p2 = Pessoa('Maykon', 25)
+
+print(p1.get_ano_nascimento())
+# p1.falar('POO')
+# p2.falar('games')
+# p1.comer('churrasco')
+# p1.falar('POO')
+# p1.comer('pão')
+# p1.parar_comer()
+# p1.parar_comer()
+# p1.comer('maçã')
 # p2 = Pessoa()
 
 
--- pessoa.py
+++ pessoa.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
+
 class Pessoa:
     # def falar(self):  # método do classe
     #     print('Pessoa está falando')
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
+
     def __init__(self, nome, idade, comendo=False, falando=False):  # função com seus parametros
         self.nome = nome
         self.idade = idade
@@ -8,5 +13,38 @@ class Pessoa:
         self.falando = falando
 
     def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome} já está comendo.')
+            return
         print(f'{self.nome} está comendo {alimento}.')
         self.comendo = True
+
+        if self.falando:
+            print(f'{self.nome} não pode comer falando.')
+            return
+
+    def parar_comer(self):
+        if not self.comendo:
+            print(f'{self.nome} não está comendo.')
+            return
+        print(f'{self.nome} parou de comer.')
+        self.comendo = False
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f'{self.nome} não pode falar comendo.')
+        if self.falando:
+            print(f'{self.nome} já está falando.')
+            return
+        print(f'{self.nome} está falando sobre {assunto}.')
+        self.falando = True
+
+    def parar_de_falar(self):
+        if not self.falando:
+            print(f'{self.nome} não está falando.')
+            return
+        print(f'{self.nome} parou de falar.')
+        self.falando = True
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
